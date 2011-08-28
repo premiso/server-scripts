3df0d9671d2ffe0905d46b7e5b2906932f3179f7
Darki <frost@darkid.darki.d>
2011-08-28 00:26:37 -0600

    Added a requirement

--- shell/organizePhotos.sh
+++ shell/organizePhotos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Requires bash 3.0
+# Requires bash 3.0 and exiftools
 # usage organizePhotos.sh [-m] [-r] [-d] [-x jpg|png|jpeg] [-i] [-p] source-path dest-path
 # Switch usages
 # 	-m  move files instead of copy (copy is the default) Optional
@@ -25,7 +25,7 @@ Switch usages:
       -r  rename files  Optional
       -x  extension (this can be any extension to match your pictures defaults to jpg) Optional
       -i  case insensitive on the extension, incase of a JPG jpg etc. Optional
-      -p  directory pattery, see strftime for valid pattern, if spaces surround in quotes.  Optional
+      -p  directory pattery, see strftime for valid pattern, if spaces surround in quotes.  Optional, but will trigger -d
 
 If spaces are in the directory, remember to use double quote around the full path IE: \"/path/to/photos of me/\" and always include ending slash.
 
@@ -33,19 +33,31 @@ Warning, use are your own risk. I take no responsability for any deleted or lost
 Also note that this has no error checking / testing. Again, use at your own risk!"
 }
 
+#Setup some defaults
+EXTENSION=jpg
+
+#Setup default pattern (see strftime)
+DEST_DIR_PATTERN="%Y_%m_%d"
+
+PAT=false
+EXTEN=false
+CREATEDIR=false
+INSENSITIVE=false
+RENAME=false
+MOVE=false
+
 # Let's gather all the items and see what needs to be done.
-for a
-do 
-	if [ $EXTEN ]; then
-		EXT=$a
-		EXTENSION=true
+for a in "$@"; do	
+	if $EXTEN ; then
+		EXT=true
+		EXTENSION="$a"
 		EXTEN=false
 		continue
 	fi
 
-	if [ $PAT ]; then
+	if $PAT ; then
 		PATTERN=true
-		PATT=$a
+		DEST_DIR_PATTERN=$a
 		PAT=false
 		continue
 	fi
@@ -69,7 +81,7 @@ do
 		-p)
 			PAT=true
 		;;
-		*)
+		-h | --help | -help)
 			help
 			exit 0
 		;;
@@ -79,27 +91,56 @@ done
 SOURCE_DIR=${BASH_ARGV[1]}
 DEST_DIR=${BASH_ARGV[0]}
 
-echo $SOURCE_DIR
-echo $DEST_DIR
-exit 0
- 
-# The date pattern for the destination dir (see strftime)
-DEST_DIR_PATTERN="%Y_%m_%d"
- 
-# Move all files having this extension
-EXTENSION=jpg
- 
-for f in `find "$SOURCE_DIR" -iname "*.$EXTENSION" -type f -mtime -10`; do
-	# Obtain the creation date from the EXIF tag
+if [ ! -d "$DEST_DIR" ]; then
+	echo "The directory $DEST_DIR does not exist, exiting."
+	exit 1
+elif [ ! -d "$SOURCE_DIR" ]; then
+	echo "The directory $SOURCE_DIR does not exist, exiting."
+	exit 1
+fi
+
+# Add ending slash.
+if [[ $DEST_DIR != */ ]]; then
+	DEST_DIR="${DEST_DIR}/"
+fi
+
+CS="-name"
+if $INSENSITIVE ; then
+	CS="-iname"
+fi
+
+i=0
+# @todo allow for custom find options, such as mtime etc.
+for f in `find "$SOURCE_DIR" $CS "*.$EXTENSION" -type f`; do
+	let i=$i+1
+
 	f_date=`exiftool $f -CreateDate -d $DEST_DIR_PATTERN | cut -d ':' -f 2 | sed -e 's/^[ \t]*//;s/[ \t]*$//'`;
- 
-	# Construct and create the destination directory
-	f_dest_dir=$DEST_DIR/$f_date
-	if [ ! -d $f_dest_dir ]; then
-		echo "Creating directory $f_dest_dir"
-		mkdir $f_dest_dir
+
+	# Obtain the creation date from the EXIF tag
+	if $CREATEDIR ; then
+		# Construct and create the destination directory
+		f_dest_dir=${DEST_DIR}$f_date
+		if [ ! -d $f_dest_dir ]; then
+			echo "Creating directory $f_dest_dir"
+			mkdir $f_dest_dir
+		fi
+	else
+		f_dest_dir=$DEST_DIR
 	fi
- 
-	mv "$f" "$f_dest_dir"
-	echo "Copied $f to $f_dest_dir"
+
+	if $RENAME ; then
+		x=`printf %03d $i`
+		f_dest_dir="${f_dest_dir}${f_date}_${x}.$EXTENSION"
+	fi 
+
+	if $MOVE ; then
+		mv "$f" "$f_dest_dir"
+		echo "Moved $f to ${f_dest_dir}${f}"
+	else
+		cp "$f" "$f_dest_dir"
+		echo "Copied $f to ${f_dest_dir}${f}"
+	fi
+
 done
+
+echo "$i images have been processed."

48fd67fd38ef8de9e1a360b61d2beadea43d00e4
Premiso <premiso@gmail.com>
2011-09-10 23:07:44 -0600

    Added the most important part

--- shell/openvzCreateCont.sh
+++ shell/openvzCreateCont.sh
@@ -14,3 +14,7 @@ if [ -e /vz/private/${cid} ]; then
 	echo "(Container exists)"
 	exit 1
 fi
+
+vzctl create ${cid} --ostemplate cd-debian-6.0-i386-minimal
+vzctl set ${cid} --ipadd 10.11.12.${cid} --hostname $2 --nameserver 8.8.8.8 --searchdomain dec.im --diskspace ${4}G --vmguarpages ${3}M --oomguarpages ${3}M --privvmpages ${3}M:${3}M --save
+vzctl start ${cid}
